a0ce36d3fdb44e39eadc948137c5883d697e1b03
cjyyy <1627814505@qq.com>
2023-06-07 11:43:47 +0800

    update

---
+++ 生成gif/main.py
@@ -0,0 +1,22 @@
+# %%
+from PIL import Image
+import os
+
+# %%
+
+# 读取文件夹中的所有png图片
+images = []
+for file_name in os.listdir('input'):
+    if file_name.endswith('.png'):
+        image = Image.open(os.path.join('input', file_name))
+        images.append(image)
+
+# %%
+
+# 根据文件名末尾数字进行排序
+images.sort(key=lambda x: int(x.filename.split('.')[1]))
+
+# %%
+
+# 合成为gif动图并导出
+images[0].save('output.gif', save_all=True, append_images=images[1:], duration=100, loop=0)
